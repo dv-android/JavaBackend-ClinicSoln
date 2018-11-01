068e6aa122cd11665236426290735bbf33801074
dv-android <devang.droid@gmail.com>
2018-11-01 23:01:29 +0530

    JSON Jackson Streaming implemented on clinicsoln/echo endpoint

--- src/main/java/com/clinicsoln/jersey/EchoEndpoint.java
+++ src/main/java/com/clinicsoln/jersey/EchoEndpoint.java
@@ -4,17 +4,32 @@ package com.clinicsoln.jersey;
 
 
 //import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 
+import org.codehaus.jackson.JsonEncoding;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.map.JsonMappingException;
+
+//import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Iterator;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.StreamingOutput;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -48,7 +63,43 @@ public class EchoEndpoint {
     @GET
     @JWTTokenNeeded
     public Response echo(@QueryParam("message") String message) {
-        return Response.ok().entity(message == null ? "no message" : message).build();
+    	
+    	 final Map<Integer, String> people  = new HashMap<>();
+         people.put(1, "Michael");
+         people.put(2, "Mark");
+         
+         StreamingOutput stream = new StreamingOutput() {
+             @Override
+             public void write(OutputStream os) throws IOException, WebApplicationException
+             {
+            	 JsonFactory jsonfactory = new JsonFactory(); 
+            	// File jsonDoc = new File(path);
+            	// JsonGenerator generator = jsonfactory.createJsonGenerator(jsonDoc, JsonEncoding.UTF8);
+
+            	// Read more: https://javarevisited.blogspot.com/2015/03/parsing-large-json-files-using-jackson.html#ixzz5VciDvoEr
+            	 
+            	 
+                 JsonGenerator jg = jsonfactory.createJsonGenerator( os, JsonEncoding.UTF8 );
+                 jg.writeStartArray();
+
+                 for ( Map.Entry<Integer, String> person : people.entrySet()  )
+                 {
+                     jg.writeStartObject();
+                     jg.writeFieldName( "id" );
+                     jg.writeString( person.getKey().toString() );
+                     jg.writeFieldName( "name" );
+                     jg.writeString( person.getValue() );
+                     jg.writeEndObject();
+                 }
+                 jg.writeEndArray();
+
+                 jg.flush();
+                 jg.close();
+             }
+         };
+
+
+         return Response.ok().entity( stream ).type( MediaType.APPLICATION_JSON ).build();
     }
 
     @GET
